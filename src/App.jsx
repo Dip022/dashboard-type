764e9ecc1c70ef3d6751ab16136c4bb774b2a3dd
Dipok Roy <roydipok022@gmail.com>
2023-11-14 10:01:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TypeOne from "./types/TypeOne/TypeOne";
+
+function App() {
+    return (
+        <div className="admin-wrapper">
+            <TypeOne />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/types/TypeOne/TypeOne.jsx
@@ -0,0 +1,17 @@
+const TypeOne = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-nav">
+                <div className="top-nav">
+                    <img src="" alt="" />
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+                <div>2</div>
+            </div>
+        </div>
+    );
+};
+
+export default TypeOne;
